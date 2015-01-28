322b5035c583eaac08fab3431679dc790f144e53
Luis Capelo <luiscape@gmail.com>
2015-01-28 10:30:51 -0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Simple script to downlaod and
+# set-up the SW folder with the necessary
+# scripts.
+
+# Compiling R.3.1
+cd ~
+wget 'http://cran.r-project.org/src/base/R-3/R-3.1.1.tar.gz'
+tar -xvf R-3.1.1.tar.gz
+mv R-3.1.1 R
+cd R
+./configure
+make
+
+# Creating Python's virtual env
+cd ~
+virtualenv venv
+
+# Cleaning-up
+rm R-3.1.1.tar.gz
