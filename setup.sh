def25f1e81d1e98c7af8246aed643fffa29eb4c6
Luis Capelo <luiscape@gmail.com>
2015-03-18 15:15:41 -0400

    updating R version

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 # set-up the SW folder with the necessary
 # scripts.
 
-# Compiling R.3.1
+# Compiling R.3.1.3
 cd ~
-wget 'http://cran.r-project.org/src/base/R-3/R-3.1.1.tar.gz'
-tar -xvf R-3.1.1.tar.gz
-mv R-3.1.1 R
+wget 'http://cran.r-project.org/src/base/R-3/R-3.1.3.tar.gz'
+tar -xvf R-3.1.3.tar.gz
+mv R-3.1.3 R
 cd R
 ./configure
 make
@@ -18,4 +18,4 @@ cd ~
 virtualenv venv
 
 # Cleaning-up
-rm R-3.1.1.tar.gz
+rm R-3.1.3.tar.gz
